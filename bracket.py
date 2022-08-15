43619ec1b528f3c6eda352cf193a1241b7a86aaa
Francis Borja <fborja4400@gmail.com>
2022-08-15 15:27:25 -0400

    feat: bracket time argument

--- bracket.py
+++ bracket.py
@@ -1,8 +1,10 @@
 import mdb
+import re
+import requests
 from discord import Embed
 from pprint import pprint
 from logger import printlog
-from datetime import datetime
+from datetime import datetime, timedelta, date
 
 # bracket.py
 # User created brackets
@@ -10,11 +12,15 @@ from datetime import datetime
 # Challonge API: https://api.challonge.com/v1
 
 BRACKETS = 'brackets'
+BASE_URL = 'https://api.challonge.com/v2'
 
-def create_bracket_embed(name, author, entrants=[]):
+time_re_long = re.compile(r'([1-9]|0[1-9]|1[0-2]):[0-5][0-9] ([AaPp][Mm])$') # ex. 10:00 AM
+time_re_short = re.compile(r'([1-9]|0[1-9]|1[0-2]) ([AaPp][Mm])$')           # ex. 10 PM
+
+def create_bracket_embed(name, author, time, entrants=[]):
     embed = Embed(title='🥊  {0}'.format(name), description="Created by {0}".format(author), color=0x6A0DAD)
-    time = datetime.now().strftime("%A, %B %d, %Y %I:%M %p") # time w/o ms
-    embed.add_field(name='Starting At', value=time, inline=False)
+    time_str = time.strftime("%A, %B %d, %Y %I:%M %p") # time w/o ms
+    embed.add_field(name='Starting At', value=time_str, inline=False)
     if len(entrants) > 0:
         entrants_content = ""
         for entrant_id in entrants:
@@ -35,24 +41,45 @@ async def add_bracket(self, message, db, argv, argc):
     if argc < 3:
         return await message.channel.send(usage)
 
-    bracket_name = ' '.join(argv[2:]) # get bracket name
+    # Parse time; Default is 1 hour from current time
+    match1 = time_re_long.search(message.content.strip()) # Check for long time
+    match2 = time_re_short.search(message.content.strip()) # Check for short time
+    if not match1 and not match2:
+        time = datetime.now() + timedelta(hours=1)
+        match = None
+    else:
+        current_time = datetime.now()
+        if match1:
+            match = match1.span()
+            time = datetime.strptime('{0} {1}'.format(date.today(), match1.group()), '%Y-%m-%d %I:%M %p')
+        elif match2:
+            match = match2.span()
+            time = datetime.strptime('{0} {1}'.format(date.today(), match2.group()), '%Y-%m-%d %I %p')
+        # Check if current time is before time on current date; If so, go to next day
+        if current_time > time:
+            time += timedelta(days=1)
+
+    # Get bracket name
+    if match:
+        argv = message.content[:match[0]].split()
+    bracket_name = ' '.join(argv[2:]) 
     # Check if bracket already exists
     bracket = await get_bracket(self, db, bracket_name)
     if bracket:
         return await message.channel.send("Bracket with name '{0}' already exists.".format(bracket_name))
 
     # Send embed message
-    embed = create_bracket_embed(bracket_name, message.author.name)
+    embed = create_bracket_embed(bracket_name, message.author.name, time)
     bracket_message = await message.channel.send(embed=embed)
+    # Add checkmark reaction to message
+    await bracket_message.add_reaction('✅')
     # Add bracket to DB
     bracket = {'name': bracket_name, 'bracket_id': bracket_message.id, 
                'author': {'username': message.author.name, 'id': message.author.id},
-               'entrants': [], 'starttime': datetime.utcnow(), 'endtime': None, 
+               'entrants': [], 'starttime': time, 'endtime': None, 
                'open': True}
-    await mdb.add_document(db, bracket, BRACKETS)
-    # Add ✅ reaction to message
-    await bracket_message.add_reaction('✅')
-    print("User '{0}' [id={1}] created new bracket '{2}'.".format(message.author.name, message.author.id, bracket_name))
+    if await mdb.add_document(db, bracket, BRACKETS):
+        print("User '{0}' [id={1}] created new bracket '{2}'.".format(message.author.name, message.author.id, bracket_name))
 
 async def delete_bracket(self, message, db, argv, argc):
     usage = 'Usage: `$bracket delete <name>`'
@@ -116,6 +143,6 @@ async def edit_bracket_message(self, bracket, message_id, channel_id):
     channel = self.get_channel(channel_id)
     message = await channel.fetch_message(message_id)
     
-    embed = create_bracket_embed(bracket['name'], bracket['author']['username'], bracket['entrants'])
+    embed = create_bracket_embed(bracket['name'], bracket['author']['username'], bracket['starttime'], bracket['entrants'])
     await message.edit(embed=embed)
 
--- test.py
+++ test.py
@@ -1,4 +1,6 @@
-from datetime import datetime
+from datetime import datetime, date, timedelta
+import re
+import requests
 
 def printlog(text):
     time = datetime.now().strftime("%Y-%m-%d %H:%M:%S") # time w/o ms
@@ -8,4 +10,25 @@ def printdate():
     time = datetime.now().strftime("%A, %B %d, %Y %I:%M %p") # time w/o ms
     print('[{0}] '.format(time))
 
-printdate()
+time_re = re.compile(r'([1-9]|0[1-9]|1[0-2]):[0-5][0-9] ([AaPp][Mm])$')
+time_re2 = re.compile(r'([1-9]|0[1-9]|1[0-2]) ([AaPp][Mm])$')
+
+test = "$bracket create Test Bracket 10:30 PM   "
+test2 = "$bracket create Test Bracket 10 PM   "
+
+# print(time_re2.search("10 PM").group())
+
+# print(time_re.search(test.strip()).span())
+# print(date.today())
+# print(datetime.now())
+# print(datetime.strptime('{0} 10:30 PM'.format(date.today()), '%Y-%m-%d %H:%M %p'))
+# time = datetime.now() + timedelta(hours=1)
+# print(time)
+
+match = time_re.search(test.strip()).span()
+
+argv = test[:match[0]].split()
+# print(argv)
+
+BASE_URL = 'https://api.challonge.com/v2'
+response = requests
